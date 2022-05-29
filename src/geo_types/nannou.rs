021020dc8494bfc596cbec7301361dbe88ccba89
ArmyOfEvilRobots <derek@armyofevilrobots.com>
2022-05-29 10:54:00 -0700

    Add L-system processing

--- src/geo_types/nannou.rs
+++ src/geo_types/nannou.rs
@@ -6,13 +6,13 @@ use num_traits::real::Real;
 // use num::traits::AsPrimitive;
 
 pub trait NannouDrawer<'a, T> {
-    fn draw_from_linestring(self, line: LineString<T>) -> Drawing<'a, Path>
+    fn polyline_from_linestring(self, line: LineString<T>) -> Drawing<'a, Path>
         where T: CoordNum, T: Real;
 }
 
 impl<'a, T> NannouDrawer<'a, T> for Drawing<'a, PathStroke>
 {
-    fn draw_from_linestring(self, line: LineString<T>) -> Drawing<'a, Path>
+    fn polyline_from_linestring(self, line: LineString<T>) -> Drawing<'a, Path>
         where T: CoordNum {
         self.points(
             line.coords()
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,19 @@
+//! ArmyOfEvilRobots pen-plotter art related tools and libraries
+//!
+//! This library contains a variety of tools used to make pen-plotter based art.
+//! While it focuses on a combination of nannou and geo/geo_types for now, it
+//! will likely expand into other areas as I find the need for my own creations.
+//! Based (extremely roughly) on [`shapely`] for Python for the geographic
+//! functions, with a splash of [`VSK`].
+//!
+//! *CAUTION: This isn't even Alpha quality yet, and I am poking away at it on
+//! my days off. May spontaneously explode, might take your plotter with it.*
+//!
+//! [`shapely`]: https://github.com/shapely/shapely
+//! [`vsk`]: https://vsketch.readthedocs.io/en/latest/index.html
+
+
 pub mod geo_types;
 pub mod turtle;
+pub mod l_system;
+
