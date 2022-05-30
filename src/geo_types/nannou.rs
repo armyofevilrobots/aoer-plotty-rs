ab25724ab371e8c0061ecaed4d06886588cf4fcd
Derek Anderson <derek.anderson@go-beep.com>
2022-05-30 08:37:37 -0700

    Minor doc updates

--- src/geo_types/nannou.rs
+++ src/geo_types/nannou.rs
@@ -26,6 +26,9 @@ use num_traits::real::Real;
 ///     .polyline_from_linestring(line)
 ///     .color(nannou::color::NAVY);
 /// ```
+///
+
+
 pub trait NannouDrawer<'a, T> {
     fn polyline_from_linestring(self, line: LineString<T>) -> Drawing<'a, Path>
         where T: CoordNum, T: Real;
