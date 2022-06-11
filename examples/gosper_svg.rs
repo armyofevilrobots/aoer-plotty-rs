6191db15270ed138d00be914621e0b2fc9f7ce07
ArmyOfEvilRobots <derek@armyofevilrobots.com>
2022-06-11 10:31:38 -0700

    Add minimal transformation and SVG trait
     - SVG stuff is still in progress.
     - Fix a hatching doctest
     - Fix an lsystem doctest
     - Various cleanup

--- examples/gosper_svg.rs
+++ examples/gosper_svg.rs
@@ -1,7 +1,10 @@
 use std::collections::HashMap;
 use geo::lines_iter::LinesIter;
 use geo::prelude::{BoundingRect, EuclideanDistance, HasDimensions, Translate};
-use geo_types::{MultiLineString, Point, Polygon};
+use geo_types::{Coordinate, CoordNum, LineString, MultiLineString, Point, Polygon, Rect};
+use geos::GeometryTypes::MultiPolygon;
+use nalgebra::{Affine2, RealField, Similarity2, Point2 as NPoint2};
+use num_traits::real::Real;
 use aoer_plotty_rs::geo_types::hatch::{Hatch, LineHatch};
 use aoer_plotty_rs::geo_types::nannou::NannouDrawer;
 use aoer_plotty_rs::geo_types::PointDistance;
@@ -12,24 +15,102 @@ use svg::Document;
 use svg::node::element::Path;
 use svg::node::element::path::Data;
 
-pub trait ToSvg<T> {
-    /// Transposes (transforms) the coords. Usually called with
-    /// -1.0, 1.0 to invert the Y dimensions before translation
-    fn transpose(self, x: T, y: T) -> Self;
 
-    /// Translates the geometry into positive SVG space. In
-    /// geo_types, higher numbers are up, negative are down.
-    /// In SVG, higher numbers are down, negative are up. This
-    /// function automatically fits the Geometry into space where
-    /// 0,0 is at the top left corner.
-    fn fit(self) -> Self;
+/// An arrangement is a plan for transformation of an SVG
+pub enum Arrangement<T>
+    where T: Real,
+          T: CoordNum,
+          T: RealField {
+    Center,
+    FitCenter,
+    Transform(Affine2<T>),
+}
+
+pub trait ToSvg<T>
+    where T: CoordNum,
+          T: Real,
+          T: RealField {
+    /// Given an [Arrangement] as a transformation strategy, transform the geometry to
+    /// fit the bounds, or just run the transformation without bounds if None
+    fn arrange(self, arrangement: Arrangement<T>, bounds: Option<Rect<T>>) -> Self;
 
     /// Utility function returns a viewbox tuple for the geometry.
-    /// Should be used AFTER transpose and fit.
-    fn viewbox(&self) -> (T,T,T,T);
+    /// Should be used AFTER calling arrange on the geo.
+    fn viewbox(&self) -> (T, T, T, T);
 
     /// Convert the Geometry into an SVG Path
     fn to_path(&self) -> Path;
+}
+
+impl<T> ToSvg<T> for MultiLineString<T>
+    where T: CoordNum,
+          T: Real,
+          T: RealField {
+
+    fn arrange(self, arrangement: Arrangement<T>, bounds: Option<Rect<T>>) -> Self {
+        let transformation = match arrangement{
+            Arrangement::Transform(affine) => affine,
+            _=> todo!()
+        };
+        let linestrings: Vec<LineString<T>> = self.iter().map(|linestring|{
+            // linestring.clone()
+            linestring.coords().map(|coord|{
+                let pt = transformation * NPoint2::<T>::new(coord.x, coord.y);
+                Coordinate::<T>::from((pt.x, pt.y))
+            }).collect()
+        }).collect();
+        MultiLineString::<T>::new(linestrings)
+    }
+
+    fn viewbox(&self) -> (T, T, T, T) {
+        todo!()
+    }
+
+    fn to_path(&self) -> Path {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::f32::consts::PI as PI_F32;
+    use std::str::FromStr;
+    use geo_types::{coord, LineString, MultiLineString};
+    use nalgebra::{Vector2, Affine2, Matrix3};
+    use wkt::Wkt;
+    use super::*;
+
+    #[test]
+    fn test_load_wkt(){
+        let geoms: Polygon::<f64> = Polygon::try_from(
+            Wkt::<f64>::from_str("POLYGON ((350 100, 450 450, 150 400, 100 200, 350 100), (200 300, 350 350, 300 200, 200 300))")
+                .expect("Failed to load WKT"))
+            .expect("Failed to load box");
+
+    }
+
+    #[test]
+    fn test_arrange_mls_arbitrary(){
+        let mls = MultiLineString::new(
+            vec![LineString::new(
+                vec![
+                    coord!{x: 0.0f64, y: 0.0f64},
+                    coord!{x: 0.0f64, y: 100.0f64},
+                    coord!{x: 100.0f64, y: 100.0f64},
+                    coord!{x: 100.0f64, y: 0.0f64},
+                    coord!{x: 0.0f64, y: 0.0f64},
+                ])]);
+        let txmls = mls.arrange(
+            Arrangement::Transform(Affine2::from_matrix_unchecked(
+                Matrix3::new(
+                    1.0, 0.0, 0.0,
+                    0.0, 1.0, 0.0,
+                    0.0, 0.0, 1.0,
+                )
+            )), None);
+        println!("TXMLS IS {:?}", txmls);
+
+    }
 }
 
 fn main() {
@@ -59,14 +140,16 @@ fn main() {
     let bounds = tlines.bounding_rect().unwrap();
     let bc = tlines.bounding_rect().unwrap().bounding_rect().center();
 
+
     // Center it
     let tlines = tlines.translate(-bc.x, -bc.y);
+
 
     let mut svg_data = Data::new();
 
     // Draw the turtle lines into the SVG context
     // tlines.iter().for_each(move |tline| {
-    for tline in tlines{
+    for tline in tlines {
         for point in tline.points().take(1) {
             svg_data = svg_data.move_to((point.x(), point.y()));
         }
@@ -85,7 +168,5 @@ fn main() {
         .add(path);
 
     svg::save("images/gosper-to-svg-example.svg", &svg).unwrap();
-
-
 }
 
