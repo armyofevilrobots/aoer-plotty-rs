a5027ce5853badb705a81efe75fbeb2ac98e2788
Derek Anderson <derek@armyofevilrobots.com>
2022-08-29 10:16:33 -0700

    Wups, and I ever embarassed. Missed stuff

---
+++ src/geo_types/matrix.rs
@@ -0,0 +1,166 @@
+use geo::map_coords::MapCoords;
+use geo_types::CoordNum;
+use geo_types::Geometry;
+use nalgebra::Matrix3;
+use nalgebra::{Affine2, Point2 as NPoint2, RealField};
+use num_traits::real::Real;
+use num_traits::{Float, Num};
+
+/// Helper to create a scaling matrix
+pub fn scale_matrix<T>(sx: T, sy: T) -> Affine2<T>
+where
+    T: RealField,
+    T: Float,
+{
+    Affine2::from_matrix_unchecked(Matrix3::<T>::new(
+        sx,
+        T::zero(),
+        T::zero(),
+        T::zero(),
+        sy,
+        T::zero(),
+        T::zero(),
+        T::zero(),
+        T::one(),
+    ))
+}
+
+/// Helper to create a translation matrix
+pub fn translate_matrix<T>(tx: T, ty: T) -> Affine2<T>
+where
+    T: RealField,
+    T: Float,
+{
+    Affine2::from_matrix_unchecked(Matrix3::<T>::new(
+        T::one(),
+        T::zero(),
+        tx,
+        T::zero(),
+        T::one(),
+        ty,
+        T::zero(),
+        T::zero(),
+        T::one(),
+    ))
+}
+
+/// Unlike in operations/context, this is standard coordinate
+/// and orientation system.
+pub fn rotate_matrix<T>(radians: T) -> Affine2<T>
+where
+    T: RealField,
+    T: Float,
+{
+    Affine2::from_matrix_unchecked(Matrix3::<T>::new(
+        Float::cos(radians),
+        Float::sin(radians).neg(),
+        T::zero(),
+        Float::sin(radians),
+        Float::cos(radians),
+        T::zero(),
+        T::zero(),
+        T::zero(),
+        T::one(),
+    ))
+}
+
+/// Unit matrix. Basically a no-op
+pub fn unit_matrix() -> Affine2<f64> {
+    Affine2::from_matrix_unchecked(Matrix3::new(1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0))
+}
+
+pub trait TransformGeometry<T: CoordNum>
+where
+    T: RealField,
+    T: Copy,
+    T: Real,
+    T: CoordNum,
+    T: Num,
+    T: Float,
+    T: RealField,
+{
+    fn xform_coord(xy: &(T, T), affine: &Affine2<T>) -> (T, T);
+    fn transformed(&self, affine: &Affine2<T>) -> Geometry<T>;
+}
+
+impl<T> TransformGeometry<T> for Geometry<T>
+where
+    T: Num,
+    T: RealField,
+    T: Float,
+{
+    /// Helper to transform geometry when we have an affine transform set.
+    fn xform_coord((x, y): &(T, T), affine: &Affine2<T>) -> (T, T) {
+        let out = affine * NPoint2::new(*x, *y);
+        (out.x, out.y)
+    }
+
+    fn transformed(&self, affine: &Affine2<T>) -> Geometry<T> {
+        self.map_coords(|xy| Self::xform_coord(xy, affine))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::prelude::PointDistance;
+    use std::f64::consts::PI;
+
+    use super::*;
+
+    #[test]
+    fn test_translate() {
+        let tx = translate_matrix(10., 5.);
+        let source = geo_types::Point::<f64>::new(25., 25.);
+        let dest = geo_types::Point::<f64>::new(35., 30.);
+        let result = Geometry::Point(source).transformed(&tx);
+        if let Geometry::Point(out) = result {
+            assert!((dest - out).length() < 1e-8);
+        } else {
+            todo!()
+        }
+    }
+
+    #[test]
+    fn test_simple_rotate() {
+        for (radians, source, destination) in vec![
+            (
+                PI / 2.,
+                geo_types::Point::new(1., 0.),
+                geo_types::Point::new(0., 1.),
+            ),
+            (
+                PI,
+                geo_types::Point::new(1., 0.),
+                geo_types::Point::new(-1., 0.),
+            ),
+            (
+                PI / 2.,
+                geo_types::Point::new(0., 1.),
+                geo_types::Point::new(-1., 0.),
+            ),
+            (
+                PI / 4.,
+                geo_types::Point::new(1., 0.),
+                geo_types::Point::new(0.7071067811865476, 0.7071067811865476),
+            ),
+            (
+                -PI / 4.,
+                geo_types::Point::new(1., 0.),
+                geo_types::Point::new(0.7071067811865476, -0.7071067811865476),
+            ),
+        ] {
+            let rotation_matrix = rotate_matrix(radians);
+            let result = Geometry::Point(source);
+            if let Geometry::Point(out) = result.transformed(&rotation_matrix) {
+                let distance = (destination - out).length();
+                // println!(
+                //     "Rotation of pt {:?} by {}\nshould have destination {:?} to actual {:?}\nFound distance {}\n\n",
+                //     &source, &radians, &destination, &out, &distance
+                // );
+                assert!(distance < 1e-8);
+            } else {
+                todo!()
+            }
+        }
+    }
+}
---
+++ src/geo_types/shapes.rs
@@ -0,0 +1,96 @@
+/// Kinda weird that arc features are missing from geo_types, but ok, here is one.
+use geo_types::{coord, Geometry, LineString, Point, Polygon};
+use num_traits::FromPrimitive;
+use std::f64::consts::PI;
+
+/// Just a regular polygon, but for some reason using compass degrees
+pub fn regular_poly(sides: usize, x: f64, y: f64, radius: f64, rotation: f64) -> Geometry<f64> {
+    // all the way around to the start again, and hit the first point twice to close it.
+    if sides < 3 {
+        return Geometry::Point(Point::new(x, y));
+    }
+
+    Geometry::Polygon(Polygon::new(
+        LineString::new(
+            (0..(sides + 2))
+                .map(|i| {
+                    let angle = rotation - PI / 2.0
+                        + (f64::from(i as i32) / f64::from(sides as i32)) * (2.0 * PI);
+                    coord! {x: x+angle.cos() * radius, y: y+angle.sin() * radius}
+                })
+                .collect(),
+        ),
+        vec![],
+    ))
+}
+
+/// Exactly the same, except uses regular mathematical orientation, radians
+pub fn regular_poly_native(
+    sides: usize,
+    x: f64,
+    y: f64,
+    radius: f64,
+    radians: f64,
+) -> Geometry<f64> {
+    // all the way around to the start again, and hit the first point twice to close it.
+    if sides < 3 {
+        return Geometry::Point(Point::new(x, y));
+    }
+
+    Geometry::Polygon(Polygon::new(
+        LineString::new(
+            (0..(sides + 2))
+                .map(|i| {
+                    let angle =
+                        radians + (f64::from(i as i32) / f64::from(sides as i32)) * (2.0 * PI);
+                    coord! {x: x+angle.cos() * radius, y: y+angle.sin() * radius}
+                })
+                .collect(),
+        ),
+        vec![],
+    ))
+}
+
+/// Draw a regular polygon with enough sides that nobody can tell the difference.
+pub fn circle(x0: f64, y0: f64, radius: f64) -> Geometry<f64> {
+    let radius = radius.abs();
+    let sides = 1000.min(32.max(usize::from_f64(radius).unwrap_or(1000) * 4));
+    regular_poly(sides, x0, y0, radius, 0.0)
+}
+
+/// Draw an arc, centered on a point. Degrees are compass degrees again, sorry.
+pub fn arc_center(x0: f64, y0: f64, radius: f64, deg0: f64, deg1: f64) -> LineString<f64> {
+    let radius = radius.abs();
+    // Clamp the angle.
+    let deg0 = PI * ((deg0 % 360.0) / 180.0);
+    let deg1 = PI * ((deg1 % 360.0) / 180.0);
+    let (deg0, deg1) = if deg0 > deg1 {
+        (deg1, deg0)
+    } else {
+        (deg0, deg1)
+    };
+    let sides = 1000.min(32.max(usize::from_f64(radius).unwrap_or(1000) * 4));
+    let segments = (deg1 - deg0) * f64::from(sides as i32).floor();
+    let seg_size = (deg1 - deg0) / segments;
+    let mut ls = LineString::<f64>::new(vec![]);
+    let mut angle = deg0;
+    for _segment in 0..(segments as i32) {
+        ls.0.push(coord! {x: x0+radius*angle.sin(), y: y0+radius*angle.cos()});
+        angle += seg_size;
+    }
+    if deg1 - angle > 0.0 {
+        ls.0.push(coord! {x: x0+radius*deg1.sin(), y: y0+radius*deg1.cos()});
+    }
+    ls
+}
+
+#[cfg(test)]
+mod test {
+    use super::arc_center;
+
+    #[test]
+    fn test_arc_c() {
+        let _arc = arc_center(0.0f64, 0.0f64, 10.0f64, 90.0f64, 135f64);
+        // println!("ARC: {:?}", &arc);
+    }
+}
